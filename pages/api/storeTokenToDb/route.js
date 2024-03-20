4c92cd542eb927a6dc4c50f6e2a4c344809e3747
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-20 16:27:41 +0500

    error solveing

---
+++ pages/api/storeTokenToDb/route.js
@@ -0,0 +1,23 @@
+import connectDb from "@app/connectDb";
+import TokenModel from "@app/models/token";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  let { tokenFromCookie, userId } = await req.json();
+  connectDb();
+  let tokenAlreadyAvaible = await TokenModel.findOne({
+    token: tokenFromCookie,
+  });
+  if (userId) {
+    await TokenModel({
+      token: tokenFromCookie,
+      userId: userId,
+    }).save();
+  } else {
+    await TokenModel.updateOne(
+      { userId: tokenAlreadyAvaible.userId },
+      { $set: { token: tokenFromCookie } }
+    );
+  }
+  return NextResponse.json({ msg: "posted token" });
+}

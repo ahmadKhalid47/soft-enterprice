597d890c2ea35357b1b1eaf50d02b1ea02a06322
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-21 18:59:36 +0500

    abcdefabcdef1234

--- app/api/storeTokenToDb/[tokenFromCookie]/[userId]/route.js
+++ app/api/storeTokenToDb/[tokenFromCookie]/[userId]/route.js
@@ -1,25 +1,25 @@
-// import connectDb from "@app/connectDb";
-// import TokenModel from "@app/models/token";
+import connectDb from "@app/connectDb";
+import TokenModel from "@app/models/token";
 import { NextResponse } from "next/server";
 
 export async function GET(req, formData) {
-  // let tokenFromCookie = formData.params.tokenFromCookie;
-  // let userId = formData.params.userId;
+  let tokenFromCookie = formData.params.tokenFromCookie;
+  let userId = formData.params.userId;
 //   let { tokenFromCookie, userId } = await req.json();
-  // connectDb();
-  // let tokenAlreadyAvaible = await TokenModel.findOne({
-  //   token: tokenFromCookie,
-  // });
-  // if (userId) {
-  //   await TokenModel({
-  //     token: tokenFromCookie,
-  //     userId: userId,
-  //   }).save();
-  // } else {
-  //   await TokenModel.updateOne(
-  //     { userId: tokenAlreadyAvaible.userId },
-  //     { $set: { token: tokenFromCookie } }
-  //   );
-  // }
+  connectDb();
+  let tokenAlreadyAvaible = await TokenModel.findOne({
+    token: tokenFromCookie,
+  });
+  if (userId) {
+    await TokenModel({
+      token: tokenFromCookie,
+      userId: userId,
+    }).save();
+  } else {
+    await TokenModel.updateOne(
+      { userId: tokenAlreadyAvaible.userId },
+      { $set: { token: tokenFromCookie } }
+    );
+  }
   return NextResponse.json({ msg: "posted token" });
 }

ee56b3cb2f9ff0ce8803c6e664c054b48c7e152b
Ahmad Raza <ahmadrazakhalid9.0@gmail.com>
2024-03-21 14:48:35 +0500

    abcdef3

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,24 +1,20 @@
-import { createToken } from "@app/auth";
+// import { createToken } from "@app/auth";
 // import connectDb from "@app/connectDb";
 import { NextResponse } from "next/server";
 // import RegistrationModel from "@app/models/registration";
 export async function GET(req) {
-  let { username, password } = await req.json();
-  let userData = { username, password };
+  // let { username, password } = await req.json();
+  // let userData = { username, password };
   // connectDb();
   // let loginData = await RegistrationModel.findOne({ username: username });
-  let loginData = {
-    _id: "abcdef",
-    password: "abcdef",
-  };
-  if (loginData) {
-    if (password === loginData.password) {
-      let token = createToken(userData);
-      return NextResponse.json({ msg: token, userId: loginData._id });
-    } else {
-      return NextResponse.json({ error: "wrong password" });
-    }
-  } else {
+  // if (loginData) {
+  //   if (password === loginData.password) {
+  //     let token = createToken(userData);
+  //     return NextResponse.json({ msg: token, userId: loginData._id });
+  //   } else {
+  //     return NextResponse.json({ error: "wrong password" });
+  //   }
+  // } else {
     return NextResponse.json({ error: "user not found" });
-  }
+  // }
 }
